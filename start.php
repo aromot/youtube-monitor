9e54fcc4ea9181eeac1a4dc1f423bbf357268094
aromot <aromot@users.noreply.github.com>
2020-06-25 11:57:57 +0200

    Added starting date & time

--- start.php
+++ start.php
@@ -5,4 +5,7 @@ echo "| YouTube Monitor |\n";
 echo "+-----------------+\n";
 echo "\n";
 
-echo "On y va les gars !\n";
+echo "Date & time: ".date('Y-m-d H:i:s')."\n";
+echo "\n";
+
+echo "Here are the channels...\n";

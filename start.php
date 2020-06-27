689753093a0c812eee36bd99a5e21b6e471007ec
aromot <aromot@gmail.com>
2020-06-27 09:35:26 +0200

    Ajout de la classe MyDateTime

--- start.php
+++ start.php
@@ -1,11 +1,17 @@
 <?php
 
+class MyDateTime extends DateTime
+{
+
+}
+
 echo "+-----------------+\n";
 echo "| YouTube Monitor |\n";
 echo "+-----------------+\n";
 echo "\n";
 
-echo "Date & time: ".date('Y-m-d H:i:s')."\n";
+$dt = new MyDateTime();
+echo "Date & time: ".dt->format('Y-m-d H:i:s')."\n";
 echo "\n";
 
 echo "Here are the channels...\n";
